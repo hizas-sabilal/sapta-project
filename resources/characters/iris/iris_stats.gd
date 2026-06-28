f8d67c975ae6c67e9356ecdec10eca97e8ed956a
unknown <hizas.sabilal@gmail.com>
2026-06-29 07:09:46 +0800

    wip: player and monster stats

---
+++ resources/characters/iris/iris_stats.gd
@@ -0,0 +1,9 @@
+class_name PlayerStats
+extends Resource
+
+@export var character_name: String = "Hero"
+@export var max_health: int = 100
+@export var max_mp: int = 50
+@export var attack: int = 10
+@export var defense: int = 8
+@export var speed: int = 10
